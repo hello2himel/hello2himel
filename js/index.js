305b8c0aa72b3c65a52ad4e3fdeaa39f27772822
Himel Das <99470657+hello2himel@users.noreply.github.com>
2023-04-20 22:41:56 +0600

    update index.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function toggleTheme () {
+  const currentTheme = document.getElementById("autoTheme").getAttribute("data-bs-theme");
+  const newTheme = currentTheme === "light" ? "dark": "light";
+  document.getElementById("autoTheme").setAttribute("data-bs-theme", newTheme);
+}
+
+function toggleThemeAuto() {
+  const systemTheme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light': 'dark';
+  document.getElementById('autoTheme').setAttribute('data-bs-theme', systemTheme);
+}
+
+// Set initial theme on page load
+toggleThemeAuto();
