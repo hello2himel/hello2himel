b14776d3ce7b80761dd79f60434b439938b49f7c
Himel Das <99470657+hello2himel@users.noreply.github.com>
2023-04-22 21:37:15 +0600

    update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function toggleTheme() {
   const newTheme = currentTheme === "light" ? "dark" : "light";
   document.getElementById("autoTheme").setAttribute("data-bs-theme", newTheme);
   localStorage.setItem("theme", newTheme);
+  updateTextEmphasis(newTheme);
 }
 
 function toggleThemeAuto() {
@@ -10,7 +11,19 @@ function toggleThemeAuto() {
   const systemTheme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'dark': 'light';
   const newTheme = savedTheme || systemTheme;
   document.getElementById('autoTheme').setAttribute('data-bs-theme', newTheme);
+  updateTextEmphasis(newTheme);
 }
 
-// Set initial theme on page load
+function updateTextEmphasis(theme) {
+  const h1Name = document.getElementById("h1Name");
+  if (theme === "light") {
+    h1Name.classList.add("text-dark");
+    h1Name.classList.remove("text-light-emphasis");
+  } else {
+    h1Name.classList.add("text-light-emphasis");
+    h1Name.classList.remove("text-dark");
+  }
+}
+
+// Set initial theme and text emphasis on page load
 toggleThemeAuto();
